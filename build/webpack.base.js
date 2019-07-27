8b030a4067d64d3ea31f0acfd97495155dd3e13e
shao <darryshaw@gmail.com>
2019-07-27 21:16:47 +0800

    edit config

---
+++ build/webpack.base.js
@@ -0,0 +1,27 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const { resolve } = require('./utils');
+
+const config = {
+  mode: process.env.NODE_ENV,
+  entry: resolve('src/main.ts'),
+  module: {
+    rules: [
+      {
+        test: /\.ts[x]?$/,
+        use: [
+          'babel-loader',
+          {
+            loader: 'ts-loader',
+            options: {
+              transpileOnly: true,
+              happyPackMode: true,
+              configFile: resolve('tsconfig.json')
+            }
+          }
+        ]
+      }
+    ]
+  }
+};
+
+module.exports = config;
